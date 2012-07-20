e257b191e3925caa6f984972d768f389a1ece442
Arjun <arjun@server>
2012-07-20 09:35:18 -0700

    asdf

--- backend/pyJit.py
+++ backend/pyJit.py
@@ -16,51 +16,27 @@ class root_node(object):
 					self.adjacencies.append(nodes.jitid)
 
 class node(object):
-	def __init__(self, row, star = False):
+	def __init__(self, row, tag = 'planet'):
 		"""
 		initializes node object
 		arg: row of SQL query data
 		"""
-		self.id = row['id']
+		self.id = row['nid']
 		self.jitid = row['jitid']
 		self.name = row['name']
-		self.value = ['value']
+		self.value = row['value']
 		self.adjacencies = []
 		self.pid = None
 		self.dct = {}
 		self.data = {}
-		self.star = star
-		self.is_star()
 		
-	def __repr__(self):
-		return self.name
-		return self.pid
-		
-	def is_star(self):
-		if self.star:
-			self.pid = 000
-			
-	def star_data(self,color_dict):
-		self.data['ticker'] = self.jitid
-		for n in self.adjacencies:
-			if n.name == 'fin info':
-				self.data['price'] = n.data['price']
-				self.data['sector'] = n.data['sector']
-				break
-		self.prep_data(color_dict)
-		
-	def planet_data(self,color_dict):
-		for n in self.adjacencies:
-			self.data[n.name] = n.value
-		self.prep_data(color_dict)
-		self.adjacencies = []
-	
 	def add_child(self,n):
 		"""
 		adds child to adjacencies list
 		"""
-		self.adjacencies.append(n)
+		self.adjacencies.append(n.id)
 		n.pid = self.id		
+		
 	def is_parent(self):
 		"""
 		checks if node has children
@@ -70,14 +46,9 @@ class node(object):
 			return True
 		else:
 			return False
-	def generate_adjacency_id(self):
-		adj_id= []
-		for childs in self.adjacencies:
-			adj_id.append(childs.jitid)
-		return adj_id
 		
 	def prep_data(self,color_dict):
-	#	self.data['dim'] = 0.7*(float(self.data['price']))
+		self.data['dim'] = 0.7*(float(self.data['price']))
 		self.data['$color'] = color_dict[self.data['sector']]
 		self.data['star'] = self.star
 		
@@ -86,18 +57,12 @@ class node(object):
 		'id':self.jitid,
 		'name': self.name,
 		'data': self.data,
-		'adjacencies':self.generate_adjacency_id()
+		'adjacencies':self.adjecencies()
 		}
 
-def create_star(jitid):
-	"""
-	creates star/company
-	args: requires companies jitID
-	return: list of all nodes in system
-	"""
-	row = get_node_by_jitid(jitid)
-	n = node(row, star = True)
-	print n.name
+def create_star(ticker):
+	row = get_node_by_jitid(ticker)
+	n = node(row, 'star')
 	system = [n]
 	if n.is_parent():
 		system.extend(create_system(n))
@@ -111,15 +76,17 @@ def create_system(parent):
 	"""
 	system = []
 	child_list = get_childs_by_pid(parent.id)
-	for childs in child_list:
-		row = get_node_by_id(childs['chid'])
-		p = node(row)
-		parent.add_child(p)
-		system.append(p)
-		if p.is_parent():
-			system.extend(create_system(p))
-		else:
-			continue
+	if parent.name != 'fin info':		
+		for childs in child_list:
+			row = get_node_by_id(childs['chid'])
+			p = node(row,'planet')
+			p.add_child(parent)
+			parent.add_child(p)
+			system.append(p)
+	else:
+		for child in  child_list:
+			parent.data[child['name']] = child['value']
+		system.append(parent)
 	return system
 
 def create_universe(companies):
@@ -129,15 +96,12 @@ def create_universe(companies):
 		universe.append(system)
 	return universe
 
-def create_data(systems, color_dict):
-	for nodes in systems:
-		if nodes.star:
-			ind = systems.index(nodes)
-		else:
-			nodes.planet_data(color_dict)
-	systems[ind].star_data(color_dict)
-	return systems
-		
+def createData(universe, color_dict):
+	for system in universe:
+		for n in system:
+			n.prep_data(color_dict)
+	return universe
+
 def create_JSON(systems):
 	for nodes in systems:
 		if nodes.pid == 000:
@@ -153,7 +117,6 @@ def create_JSON(systems):
 
 def write_json(universe, color_dict):
 	for systems in universe:
-		systems = create_data(systems, color_dict)
 		create_JSON(systems)
 
 def main(companies, color_dict):
@@ -163,5 +126,5 @@ def main(companies, color_dict):
 	prints json structure of node
 	"""
 	universe = create_universe(companies)
-	print universe
-	# write_json(universe, color_dict)
+	universe1 = createData(universe, color_dict)
+	write_json(universe1)
