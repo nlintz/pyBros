8c752c19ff4756f0e50d50021c4ab6790530a2b6
Nathan Lintz <nathan_lintz@students.olin.edu>
2012-07-20 09:28:42 -0700

    dummy data

--- mvp.js
+++ mvp.js
@@ -29,11 +29,102 @@ function init() {
 	var json =
 			[
 			{
-			"adjacencies": ["239"], "data": {"is_star":true,"value": null, "name":"intuit","jitid":"herroi","value":"9000"}, "name": "intuit", "id": "Az9"
+			"adjacencies": ["1","2","3","4","5","6","7","8","9"], "data": {$dim:300, $color:"grey", "is_star":true,"value": null, "name":"IM A BLACK HOLE","jitid":"","value":""}, "name": "intuit", "id": "0"
 			},
 			{
-				"adjacencies": ["Az9"], "data": {"is_star":false,"value": null, "name":"mars","jitid":"","value":"", "fin info":"100"}, "name": "intuit", "id": "239"
-			}
+			"adjacencies": ["0","10","11"], "data": {$color:"D0D9C8",$dim:100,is_star:true,"value": "100", "name":"mars","jitid":"MA","fin info":"100"}, "name": "mars", "id": "1"
+			},
+			{
+			"adjacencies": ["0","20","21"], "data": {$color:"FDBD27",$dim:75,is_star:true,"value": "150", "name":"pluto","jitid":"Pl","fin info":"100"}, "name": "pluto", "id": "2"
+			},
+			{
+				"adjacencies": ["0","30","31"], "data": {$color:"DA5915",$dim:200,"is_star":true,"value": "150", "name":"saturn","jitid":"S","fin info":"100"}, "name": "saturn", "id": "3"
+			},
+			{
+				"adjacencies": ["0","40","41"], "data": {$color:"4813A4",$dim:140,"is_star":true,"value": "200", "name":"uranus","jitid":"U","fin info":"100"}, "name": "uranus", "id": "4"
+			},
+			{
+				"adjacencies": ["0","50","51"], "data": {$color:"1581F5",$dim:59,"is_star":true,"value": "10", "name":"venus","jitid":"V","fin info":"100"}, "name": "venus", "id": "5"
+			},
+			{
+				"adjacencies": ["0","60","61"], "data": {$color:"333333",$dim:80,"is_star":true,"value": "1500", "name":"neptune","jitid":"NE","fin info":"100"}, "name": "neptune", "id": "6"
+			},
+			{
+				"adjacencies": ["0","70","71"], "data": {$color:"5FFF00",$dim:60,"is_star":true,"value": "1950", "name":"HG","jitid":"Merc","fin info":"100"}, "name": "HG", "id": "7"
+			},
+			{
+				"adjacencies": ["0","80","81"], "data": {$color:"808099",$dim:25,is_star:true,"value": "20", "name":"Jupiter","jitid":"JU","fin info":"100"}, "name": "Jupiter", "id": "8"
+			},
+			{
+				"adjacencies": ["0","90","91"], "data": {$color:"003399",$dim:130,"is_star":true,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"100"}, "name": "NathanVille", "id": "9"
+			},
+			
+			
+			{
+				"adjacencies": ["1"], "data": {$dim:60,"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"Stock Went up 100!"}, "name": "NathanVille", "id": "10"
+			},
+			{
+				"adjacencies": ["1"], "data": {$dim:60,"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"New CEO!"}, "name": "NathanVille", "id": "11"
+			},
+			
+			{
+				"adjacencies": ["2"], "data": {$dim:30,"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"Stock Went up 100!"}, "name": "NathanVille", "id": "20"
+			},
+			{
+				"adjacencies": ["2"], "data": {"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"New CEO!"}, "name": "NathanVille", "id": "21"
+			},
+			
+			{
+				"adjacencies": ["3"], "data": {$dim:30,"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"Stock Went up 100!"}, "name": "NathanVille", "id": "30"
+			},
+			{
+				"adjacencies": ["3"], "data": {$dim:60,"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"New CEO!"}, "name": "NathanVille", "id": "31"
+			},
+			
+			{
+				"adjacencies": ["4"], "data": {"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"Stock Went up 100!"}, "name": "NathanVille", "id": "40"
+			},
+			{
+				"adjacencies": ["4"], "data": {$dim:30,"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"New CEO!"}, "name": "NathanVille", "id": "41"
+			},
+			
+			{
+				"adjacencies": ["5"], "data": {"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"Stock Went up 100!"}, "name": "NathanVille", "id": "50"
+			},
+			{
+				"adjacencies": ["5"], "data": {$dim:60,"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"New CEO!"}, "name": "NathanVille", "id": "51"
+			},
+			
+			{
+				"adjacencies": ["6"], "data": {"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"Stock Went up 100!"}, "name": "NathanVille", "id": "60"
+			},
+			{
+				"adjacencies": ["6"], "data": {$dim:30,"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"New CEO!"}, "name": "NathanVille", "id": "61"
+			},
+			
+			{
+				"adjacencies": ["7"], "data": {"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"Stock Went up 100!"}, "name": "NathanVille", "id": "70"
+			},
+			{
+				"adjacencies": ["7"], "data": {$dim:60,"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"New CEO!"}, "name": "NathanVille", "id": "71"
+			},
+			
+			{
+				"adjacencies": ["8"], "data": {"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"Stock Went up 100!"}, "name": "NathanVille", "id": "80"
+			},
+			{
+				"adjacencies": ["8"], "data": {$dim:30,"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"New CEO!"}, "name": "NathanVille", "id": "81"
+			},
+			
+			{
+				"adjacencies": ["9"], "data": {$dim:60,"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"Stock Went up 100!"}, "name": "NathanVille", "id": "90"
+			},
+			{
+				"adjacencies": ["9"], "data": {$dim:30,"is_star":false,"value": "160", "name":"NathanVille","jitid":"NV","fin info":"New CEO!"}, "name": "NathanVille", "id": "91"
+			},
+			
+			
+			
 			]
 	
 			
@@ -64,7 +155,6 @@ function init() {
 		},
 		// Set Node and Edge styles.
 		Node : {
-			color : '#0099FF',
 			alpha : 1,
 			'overridable' : true,
 			allowVariableNodeDiameters : true,
