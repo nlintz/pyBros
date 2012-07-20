72a1058c0d394d337e2d36b1ffd1fad8ae168a62
trevor <trevor@qwed.(none)>
2012-07-20 00:24:54 -0700

    Adding slitherer, not yet working though

---
+++ slitherer/eat.py
@@ -0,0 +1,16 @@
+###############################################
+# eat.py
+#
+# This file will work on feeding processed data
+#  to the database, it may need some utensils
+#
+# 2012-07-19
+# Trevor Pottinger
+###############################################
+
+class database_feeder(object):
+	def __init__(self, uname, passwd, dbname):
+		pass
+
+	def eat(self, data):
+		pass
