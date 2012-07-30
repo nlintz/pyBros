bcfa348ef57aaa521a158c8f409565e42c4b5b4b
Nathan Lintz <nathan_lintz@students.olin.edu>
2012-07-29 19:11:24 -0700

    blaahh

--- newGraphApi/gamescript.js
+++ newGraphApi/gamescript.js
@@ -28,18 +28,34 @@ function gamescript(){
 	function draw() {
 		canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 		var angleOffset=0;
+		
 		Stars.forEach(function(star){
 			var angle = calculateAngles(Stars)*Math.PI/180
 			star.draw(angle*angleOffset);
 			angleOffset++;
+			
 		});
-		b.draw()
+		Planets.forEach(function(planet){
+			var angle=Math.PI;
+			planet.draw(angle+angleOffset);
+			angleOffset++;
+		});
 		
+		b.draw();
 	}
-	var s = new Star({size:50,color:'green',distance:100})
-	var s2 = new Star({size:50,color:'yellow',distance:100})
-	var s3 = new Star({size:35, color:'blue', distance: 100})
-	var Stars = [s,s2,s3];
+	
+	var Planets = [];
+	var s = new Star({size:50,color:'green',distance:100, planets:[p]})
+	var s2 = new Star({size:50,color:'yellow',distance:100,planets:['p2','p3']})
+// 	var s3 = new Star({size:35, color:'blue', distance: 100})
+// 	var s4 = new Star({size:35, color:'blue', distance: 100})
+// 	var Stars = [s,s2,s3,s4];
+	var Stars = [s,s2];
+	
+// 	var p = new Planet(Stars[0]);
+// 	var p2 = new Planet(Stars[1]);
+// 	var Planets = [p,p2];
+	
 	var b = new BlackHole({size:20})
 	
 //	collision logic
@@ -54,12 +70,19 @@ function gamescript(){
 // 		this.ticker=sunObject.ticker
 // 		this.value=sunObject.value
 // 		this.adjacencies=sunObject.adjacencies;
+		
+
+		var planets = sunObject.planets;
+		
+		
+		
 		this.distance=sunObject.distance;
 		this.color=sunObject.color
 		this.size=sunObject.size
 		var rotationHolder = 0;
 		this.x=this.distance*Math.cos(angle+rotationHolder)+CENTERX;
 		this.y=this.distance*Math.sin(angle+rotationHolder)+CENTERY;
+		
 		this.draw = function(angle){
 				canvas.beginPath();
 				var iangle=0;
@@ -70,11 +93,33 @@ function gamescript(){
 				canvas.closePath();
 				canvas.fillStyle=this.color;
 				canvas.fill();
-				rotationHolder += .002;
+				rotationHolder += .02;
 			};
-		}
+			
+		planets.forEach(function(planet){
+			Planets.push(planet)
+		});
+	}
 	
-// 	function Planet(
+	function Planet(starOrigin){
+		var planetaryOffset = starOrigin.size+25;
+		var rotationHolder = 0;
+
+		this.draw = function(angle){
+			this.x=planetaryOffset*Math.sin(angle+rotationHolder)+starOrigin.x;
+			this.y=planetaryOffset*Math.cos(angle+rotationHolder)+starOrigin.y;
+			console.log(this.x)
+			canvas.beginPath();
+			var iangle=0;
+			var eangle=Math.PI*2;
+			canvas.arc(this.x,this.y,10,iangle,eangle,true);
+			canvas.closePath();
+			canvas.fillStyle='black';
+			canvas.fill();
+			rotationHolder += .05
+			
+		}
+	}
 		
 	function BlackHole(sunObject){
 // 		this.name=sunObject.name
