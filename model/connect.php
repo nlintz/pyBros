f2ea6a99550e1322d15f97c108d59e1f4719dee2
Trevor Pottinger <tpott3@gmail.com>
2012-07-31 23:07:09 +0000

    Updating model layer with php rather than python

---
+++ model/connect.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * SERVERNAME	localhost
+ * USERNAME		pybros
+ * PASSWORD		getIn2it
+ * DATABASE		pybros
+ */
+
+/**
+ * Returns a mysql connection or dies if error occurs
+ */
+function connect() {
+	$con = mysql_connect('localhost', 'pybros', 'getIn2it');
+
+	if (!$con) {
+		die('Could not connect to database: ' . mysql_error());
+	}
+
+	mysql_select_db('pybros', $con);
+
+	return $con;
+}
+
+/**
+ * Closes a mysql connection
+ */
+function disconnect($connection) {
+	mysql_close($connection);
+}
+
+?>
