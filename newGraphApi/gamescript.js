a6cbcd65eaa29f4ec384fdb8e9e42ac30f98844b
Nathan Lintz <nathan_lintz@students.olin.edu>
2012-08-08 09:20:54 -0700

    gonna try to fix the bindings

--- newGraphApi/gamescript.js
+++ newGraphApi/gamescript.js
@@ -8,6 +8,16 @@ function gamescript(){
 			+ window.innerHeight + "'></canvas>");
 	var canvas = canvasElement.get(0).getContext("2d");
 	canvasElement.appendTo('body');
+	
+// 	Kinetic JS Elements
+
+	var stage = new Kinetic.Stage({
+          container: "container",
+          width: window.innerWidth,
+          height: window.innerHeight
+        });
+    var layer = new Kinetic.Layer();
+
 // End Setting Up The Canvas
 
 
@@ -26,41 +36,42 @@ function gamescript(){
 
 //	Update Loop - Controls game logic IE carcurations
 	function update() {
-	}
-	
-//	Draw Loop - clears and repaints the screen every frame
-	
-	function draw() {
-// 		Resize the canvas to 100%
-	
-	
-		canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 		var angleOffset=0;
 		
 		Stars.forEach(function(star){
 			var angle = calculateAngles(Stars)
 			star.draw(angle*angleOffset);
 			angleOffset++;
-
-			
 		});
 		Planets.forEach(function(planet){
 			var angle=calculateAngles(planet.starOrigin.planets);
 			planet.draw(angle*angleOffset);
 			angleOffset++;
-
 		});
-		
+	}
+	
+//	Draw Loop - clears and repaints the screen every frame
+	
+	function draw() {	
+		canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 		b.draw();
+		stage.add(layer);
+		stage.remove(layer);
 	}
 	
 // 	Initialize the objects 
 // 	%TODO% make an interface to enter in all the objects from a json object
-	var s = new Star({size:50,color:'green',distance:200, name: "apple", price: "10", ticker:"AAPL"})
-	var s2 = new Star({size:50,color:'yellow',distance:250, name: "starbucks", price: "20", ticker:"SB"})
-	var s3 = new Star({size:35, color:'blue', distance: 140, name: "microsoft", price: "30", ticker:"MC"})
-	var s4 = new Star({size:35, color:'blue', distance: 300, name: "ubisoft", price: "100", ticker:"UB"})
-	var Stars = [s,s2,s3,s4];
+// 	var s = new Star({size:50,color:'green',distance:200, name: "apple", price: "10", ticker:"AAPL", orbitalVelocity:.02})
+// 	var s2 = new Star({size:50,color:'yellow',distance:140, name: "starbucks", price: "20", ticker:"SB", orbitalVelocity:.05})
+// 	var s3 = new Star({size:35, color:'blue', distance: 140, name: "microsoft", price: "30", ticker:"MC", orbitalVelocity:.01})
+// 	var s4 = new Star({size:35, color:'blue', distance: 300, name: "ubisoft", price: "100", ticker:"UB", orbitalVelocity:.01})
+// 	var Stars = [s,s2,s3,s4];
+
+	Stars=[]
+	for (var i=0; i<50; i++){
+		Stars.push(new Star({size:50,color:'green',distance:200+i*10, name: "apple", price: "10", ticker:"AAPL", orbitalVelocity:.02}))
+	};
+	console.log(Stars);
 	
 	var p = new Planet(Stars[0]);
 	var p2 = new Planet(Stars[1]);
@@ -73,6 +84,7 @@ function gamescript(){
 	var Planets = [p,p2,p3,p4,p5];
 	
 	var b = new BlackHole({size:20})
+	b.draw();
 // 	Objects initialized
 	
 	
@@ -89,7 +101,7 @@ function gamescript(){
 		this.price=sunObject.price
 // 		this.adjacencies=sunObject.adjacencies;
 		this.planets=[];		
-		
+		this.orbitalVelocity = sunObject.orbitalVelocity;
 		this.distance=sunObject.distance;
 		this.color=sunObject.color
 		this.size=sunObject.size
@@ -98,17 +110,37 @@ function gamescript(){
 		this.y=this.distance*Math.sin(angle+rotationHolder)+CENTERY;
 		this.text=this.name+ "::" + this.ticker +"::" + "$" + this.price;
 		this.draw = function(angle){
-				canvas.beginPath();
-				var iangle=0;
-				this.x = this.distance*Math.cos(angle+rotationHolder)+CENTERX;
-				this.y = this.distance*Math.sin(angle+rotationHolder)+CENTERY;
-				var eangle=Math.PI*2;
-				canvas.arc(this.x,this.y,this.size,iangle,eangle, true);
-				canvas.closePath();
-				canvas.fillStyle=this.color;
-				canvas.fill();
-				rotationHolder += .02;
-
+// 				canvas.beginPath();
+// 				var iangle=0;
+// 				var eangle=Math.PI*2;
+// 				canvas.arc(this.x,this.y,this.size,iangle,eangle, true);
+// 				canvas.closePath();
+// 				canvas.fillStyle=this.color;
+// 				canvas.fill();
+// 				rotationHolder += .02;
+			this.x = this.distance*Math.cos(angle+rotationHolder)+CENTERX;
+			this.y = this.distance*Math.sin(angle+rotationHolder)+CENTERY;
+			var label = new Kinetic.Text({
+          		x: this.x,
+          		y: this.y,
+          		text: this.name+"\n"+this.ticker+"\n"+this.price,
+          		fontSize: 12,
+          		fontFamily: "Arial",
+          		textFill: "gray"
+        	});
+			var star = new Kinetic.Circle({
+          		x: this.x,
+          		y: this.y,
+          		radius: this.size,
+          		fill: this.color,
+       	 	});
+       	 	star.on('click', function(){
+       	 		console.log('hello, world!');
+       	 	});
+       	 	
+       		rotationHolder += this.orbitalVelocity;
+			layer.add(star);
+			layer.add(label);
 			};
 		}
 	
@@ -118,41 +150,66 @@ function gamescript(){
 		var rotationHolder = 0;
 		starOrigin.planets.push(this);
 		this.draw = function(angle){
-			canvas.beginPath();
-			var iangle=0;
-			this.x=planetaryOffset*Math.cos(angle+rotationHolder)+starOrigin.x;
+// 		This was how i implemented circles before, its clearly not the right way to do it
+// 			canvas.beginPath();
+// 			var iangle=0;
+// 			this.x=planetaryOffset*Math.cos(angle+rotationHolder)+starOrigin.x;
+// 			this.y=planetaryOffset*Math.sin(angle+rotationHolder)+starOrigin.y;
+// 			var eangle=Math.PI*2;
+// 			canvas.arc(this.x,this.y,10,iangle,eangle,true);
+// 			canvas.closePath();
+// 			canvas.fillStyle='red';
+// 			canvas.fill();
 
-			this.y=planetaryOffset*Math.sin(angle+rotationHolder)+starOrigin.y;
-			
-			var eangle=Math.PI*2;
-			canvas.arc(this.x,this.y,10,iangle,eangle,true);
-			canvas.closePath();
-			canvas.fillStyle='red';
-			canvas.fill();
-
-			rotationHolder += .05
-			
+		var planet = new Kinetic.Circle({
+        	x: planetaryOffset*Math.cos(angle+rotationHolder)+starOrigin.x,
+        	y: planetaryOffset*Math.sin(angle+rotationHolder)+starOrigin.y,
+        	radius: 10,
+        	fill: "red",
+			});
+       	rotationHolder += .05
+		layer.add(planet);
 		}
+		
 	}
 		
 	function BlackHole(sunObject){
 // 		this.name=sunObject.name
 // 		this.adjacencies=sunObject.adjacencies;
-		
+		this.color='gray'
 		this.distance=sunObject.distance;
 		this.size=100;
 		this.draw = function(angle){
-				canvas.beginPath();
-				var iangle=0;
-				var x = CENTERX
-				var y = CENTERY;
-				var eangle=Math.PI*2;
-				canvas.arc(x,y,this.size,iangle,eangle, true);
-				canvas.closePath();
-				canvas.fillStyle='black';
-				canvas.fill();
-				canvas.fillStyle='white';
-				canvas.fillText('Im a fucking BLACKHOLE',x-90,y);
+// 				canvas.beginPath();
+// 				var iangle=0;
+// 				var x = CENTERX
+// 				var y = CENTERY;
+// 				var eangle=Math.PI*2;
+// 				canvas.arc(x,y,this.size,iangle,eangle, true);
+// 				canvas.closePath();
+// 				canvas.fillStyle='black';
+// 				canvas.fill();
+// 				canvas.fillStyle='white';
+// 				canvas.fillText('Im a fucking BLACKHOLE',x-90,y);
+			this.x = CENTERX;
+			this.y = CENTERY;
+			var blackHoleLabel = new Kinetic.Text({
+          		x: this.x-99,
+          		y: this.y,
+          		text: "Im a fucking black hole",
+          		fontSize: 14,
+          		fontFamily: "Arial",
+          		textFill: "white"
+        	});
+			var blackhole = new Kinetic.Circle({
+          		x: this.x,
+          		y: this.y,
+          		radius: this.size,
+          		fill: this.color,
+       	 	});
+			layer.add(blackhole);
+			layer.add(blackHoleLabel);
+
 			};
 		}
 		
