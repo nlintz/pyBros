4e01bc6aae5f2365a92056b46be313f3178a4c91
Trevor Pottinger <tpott3@gmail.com>
2012-08-08 17:23:40 +0000

    Cleaned with fire

--- old/backend/runJit.py
+++
@@ -1,5 +0,0 @@
-#pyJit caller
-import py2jit
-companies = ['CAG', 'AAPL']
-color_dict = {'Industries':'D0D9C8','Financials':'FDBD27','Healthcare':'DA5915','Consumer Discretionary':'4813A4','Utilities':'1581F5','Information Technology':'333333','Materials':'5FFF00','Energy':'808099','Consumer Staples':'003399','Telecom':'CCCCCC'}
-py2jit.main(companies, color_dict)
--- old/testDb.py
+++
@@ -1,2 +0,0 @@
-import _mysql
-db = _mysql.connect(host="172.17.178.59", user = "arjun", passwd = "pybros", db = "rev1")
